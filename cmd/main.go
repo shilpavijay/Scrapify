d1a59db7fd93c5a50beb178b1858d1c47d98f151
Shilpa AB <shilpa.amberker@gmail.com>
2024-05-22 22:53:24 +0530

    scraper func completed

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func fetchHTML(url string) (*html.Node, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return doc, nil
+}
+
+func traverseHTML(n *html.Node, targetElement, targetClass string) {
+	if n.Type == html.ElementNode && n.Data == targetElement {
+		for _, a := range n.Attr {
+			if a.Key == "class" && strings.Contains(a.Val, targetClass) {
+
+				fmt.Println(extractText(n))
+				break
+			}
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		traverseHTML(c, targetElement, targetClass)
+	}
+}
+
+func extractText(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	var text string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		text += extractText(c)
+	}
+	return text
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	url := "https://www.chrismytton.com/plain-text-websites/"
+
+	doc, err := fetchHTML(url)
+	if err != nil {
+		fmt.Printf("Error fetching HTML: %v\n", err)
+		return
+	}
+
+	targetElement := "div"
+	targetClass := "post"
+	traverseHTML(doc, targetElement, targetClass)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/shilpavijay/Scrapify.git
 
 go 1.22.3
+
+require golang.org/x/net v0.25.0 // indirect
